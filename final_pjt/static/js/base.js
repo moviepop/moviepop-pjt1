87bfdf928111b799c052e13f5b31eca863f7a1fc
Jo-Myounghee <whaudg98@naver.com>
2020-12-21 21:45:21 +0900

    [feat] 상단 바 검색 기능 아이콘 수정

---
+++ final_pjt/static/js/base.js
@@ -0,0 +1,18 @@
+searchImgs = document.querySelectorAll('.search-img')
+searchBars = document.querySelectorAll('.search-bar')
+
+function showSearchBar() {
+  searchBars.forEach(searchBar => searchBar.classList.toggle('d-none'))
+
+  // searchImg.setAttribute('class', 'd-none')
+  // searchBar.removeAttribute('class', 'd-none')
+}
+
+window.onkeydown = function(event) {
+  if(event.keyCode == 27) {
+    searchImgs.forEach(searchImg => searchImg.removeAttribute('class', 'd-none'))
+    searchBars.forEach(searchBar => searchBar.setAttribute('class', 'd-none'))
+  }
+}
+
+searchImgs.forEach(searchImg => addEventListener('click', showSearchBar))
