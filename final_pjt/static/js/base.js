1c21eec0d706d2e240791c12325c4ae8db7f5a82
Jo-Myounghee <whaudg98@naver.com>
2020-12-22 19:53:01 +0900

    [fix] 상단바의 검색 버튼 눌렀을 때 검색 아이콘 사라지지 않게 변경

--- final_pjt/static/js/base.js
+++ final_pjt/static/js/base.js
@@ -2,17 +2,12 @@ searchImgs = document.querySelectorAll('.search-img')
 searchBars = document.querySelectorAll('.search-bar')
 
 function showSearchBar() {
-  searchImgs.forEach(searchImg => searchImg.classList.toggle('d-none'))
   searchBars.forEach(searchBar => searchBar.classList.toggle('d-none'))
-
-  // searchImg.setAttribute('class', 'd-none')
-  // searchBar.removeAttribute('class', 'd-none')
 }
 
 window.onkeydown = function(event) {
-  if(event.keyCode == 27) {
-    searchImgs.forEach(searchImg => searchImg.removeAttribute('class', 'd-none'))
-    searchBars.forEach(searchBar => searchBar.setAttribute('class', 'd-none'))
+  if(event.keyCode == 27 && !searchBars.forEach(searchBar => searchBar.getAttribute('class', 'd-none'))) {
+    searchBars.forEach(searchBar => searchBar.classList.toggle('d-none'))
   }
 }
 
